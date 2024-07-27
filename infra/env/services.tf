4105b65e975d2f52cfc7ed142ced540a042f089f
Shunsuke Tsumori <tsumori.shunsuke@gmail.com>
2024-07-27 12:32:30 +0900

    initial terraform resources

---
+++ infra/env/services.tf
@@ -0,0 +1,13 @@
+locals {
+  services = toset([
+    "run.googleapis.com",
+    "cloudbuild.googleapis.com",
+  ])
+}
+
+resource "google_project_service" "project_services" {
+  for_each = local.services
+
+  service            = each.key
+  disable_on_destroy = false
+}
